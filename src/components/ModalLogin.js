8ef7427c66002843cae5d7cc9068bd2e8097fa61
KYLIAN <kylian@MacBook-Pro-de-mac-2.local>
2019-11-22 12:05:29 +0100

    edit modal

---
+++ src/components/ModalLogin.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Image,
+    Modal,
+    TouchableHighlight,
+    TouchableOpacity
+
+} from 'react-native';
+
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import styles from '../style/Style';
+import Button from '../components/Button';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+export default class ModalLogin extends React.Component {
+    state = {
+        modalVisible: false,
+    };
+
+    setModalVisible(visible) {
+        this.setState({ modalVisible: visible });
+    }
+    render() {
+        return (
+            <View style={[{ marginTop: 22 }]}>
+                <Modal animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}>
+                    <View style={[styles.backgroundApp, { alignItems: "center" }]}>
+                        <View style={[{ marginTop: 20 }, styles.container]}>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    this.setModalVisible(!this.state.modalVisible);
+                                }}>
+                                <Icon name="times" size={25} style={{ color: "white" }}></Icon>
+                            </TouchableOpacity>
+                            <View style={{ marginTop: 200, }}>
+                                <Button icon={<Icon name="google" size={20} style={{ color: '#00787E', marginRight: 10, position: "absolute", right: 80, top: -10 }}></Icon>} color="#00787E" look="primary" content="Google" />
+                                <Button icon={<Icon name="facebook" size={20} style={{ color: '#00787E', marginRight: 10, position: "absolute", right: 75, top: -10 }}></Icon>} color="#00787E" look="primary" content="Facebook" />
+                                <Button color="white" look="secondary" content="Create account" />
+                            </View>
+
+                        </View>
+                    </View>
+                </Modal>
+                <TouchableOpacity
+                    onPress={() => {
+                        this.setModalVisible(true);
+                    }}>
+                    <Text style={{ color: "white", fontSize: 18, }}>More option</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+}
--- src/screens/LoggedOut.js
+++ src/screens/LoggedOut.js
@@ -24,6 +24,7 @@ import {
 import styles from '../style/Style';
 import Button from '../components/Button';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import ModalLogin from '../components/ModalLogin'
 
 
 export default class LoggedOut extends React.Component {
@@ -51,40 +52,12 @@ export default class LoggedOut extends React.Component {
                             <Button icon={<Icon name="google" size={20} style={{ color: '#00787E', marginRight: 10, position: "absolute", right: 20, top: -10 }}></Icon>} color="#00787E" look="primary" content="Continue with Google" />
                             <Button color="white" look="secondary" content="Create account" />
                         </View>
-                        <View style={[{ marginTop: 22 }]}>
-                            <Modal
-                                animationType="slide"
-                                transparent={false}
-                                visible={this.state.modalVisible}
-                            >
-                                <View style={[styles.backgroundApp, { alignItems: "center" }]}>
-                                    <View style={[{ marginTop: 20 }, styles.container]}>
-                                        <TouchableOpacity
-                                            onPress={() => {
-                                                this.setModalVisible(!this.state.modalVisible);
-                                            }}>
-                                            <Icon name="times" size={25} style={{ color: "white" }}></Icon>
-                                        </TouchableOpacity>
-                                        <View style={{ marginTop: 200, }}>
-                                            <Button icon={<Icon name="google" size={20} style={{ color: '#00787E', marginRight: 10, position: "absolute", right: 80, top: -10 }}></Icon>} color="#00787E" look="primary" content="Google" />
-                                            <Button icon={<Icon name="facebook" size={20} style={{ color: '#00787E', marginRight: 10, position: "absolute", right: 75, top: -10 }}></Icon>} color="#00787E" look="primary" content="Facebook" />
-                                            <Button color="white" look="secondary" content="Create account" />
-                                        </View>
-
-                                    </View>
-                                </View>
-                            </Modal>
-                            <TouchableOpacity
-                                onPress={() => {
-                                    this.setModalVisible(true);
-                                }}>
-                                <Text style={{ color: "white", fontSize: 18, }}>More option</Text>
-                            </TouchableOpacity>
-                        </View>
+                        <ModalLogin />
                         <Text style={[styles.textWhite, { marginTop: 20, opacity: 0.7 }]}>We use cookies to personalize our content and advertising, measure the effectiveness of our advertising and provide a safer experience.</Text>
                     </View>
-
                 </View>
+
+
             </>
         )
     }
