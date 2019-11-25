d16a26aba204d7eb98c0581db4c8ee028ccce599
KYLIAN <kylian@MacBook-Pro-de-mac-2.local>
2019-11-25 18:01:30 +0100

    add textInput Sae

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -16,6 +16,9 @@ import {
 
 import styles from '../style/Style';
 import Header from '../components/Header';
+import { Sae } from 'react-native-textinput-effects';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+
 
 
 export default class Login extends React.Component {
@@ -35,17 +38,40 @@ export default class Login extends React.Component {
         return (
             <View style={[{ paddingTop: 22, flex: 1, backgroundColor: "#00787E" }]}>
                 <View style={styles.container}>
-                    <Text style={[{ marginTop: 30, fontSize: 24 }, styles.textWhite]}>Connexion</Text>
-                    <TextInput
-                        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                        onChangeText={text => onChangeText(text)}
-                        value="Mail address"
+                    <Text style={[{ marginTop: 50, fontSize: 24 }, styles.textWhite]}>Connexion</Text>
+
+                    <Sae
+                        label={'Email Address'}
+                        iconClass={FontAwesomeIcon}
+                        iconName={'pencil'}
+                        iconColor={'white'}
+                        inputPadding={16}
+                        labelHeight={24}
+                        // active border height
+                        borderHeight={2}
+                        // TextInput props
+                        autoCapitalize={'none'}
+                        autoCorrect={false}
+                        onChangeText={(text) => { this.setState({ textValue: text }) }}
+                        labelStyle={{ color: "white", fontSize: 22 }}
+                        style={{ marginVertical: 15 }}
                     />
-                    <TextInput
-                        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                        onChangeText={text => onChangeText(text)}
-                        value="Password"
+                    <Sae
+                        label={'Password'}
+                        iconClass={FontAwesomeIcon}
+                        iconName={'lock'}
+                        iconColor={'white'}
+                        inputPadding={16}
+                        labelHeight={24}
+                        // active border height
+                        borderHeight={2}
+                        // TextInput props
+                        autoCapitalize={'none'}
+                        autoCorrect={false}
+                        onChangeText={(text) => { this.setState({ textValue: text }) }}
+                        labelStyle={{ color: "white", fontSize: 22 }}
                     />
+
                 </View>
             </View>
         );
