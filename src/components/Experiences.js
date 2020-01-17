e575ee5f9e201ac62ea0a6e6eda3cc1eec4fe16e
Kylian <kylian.petitgenet@gmail.com>
2020-01-17 20:08:54 +0100

    add likes with toggle icon

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -6,14 +6,10 @@ import {
     Text,
 } from 'react-native';
 
-
-import Icon from 'react-native-vector-icons/FontAwesome';
-
-
-
-
 import styles from '../style/Style';
 import Stars from './Stars';
+import Like from './Like';
+
 
 export default class Experiences extends React.Component {
 
@@ -27,10 +23,12 @@ export default class Experiences extends React.Component {
                             return (
                                 <View>
                                     <Image
-                                        icon={< Icon name="heart-o" size={20} style={{ color: '#fffff', position: "absolute", right: 20, top: -10, zIndex: 9999 }}></Icon >}
-                                        style={[{ width: 150, height: 100, marginRight: 20, position: "relative" }, styles.card]}
+
+                                        style={[{ width: 150, height: 100, marginRight: 20, position: "relative", zIndex: 1 }, styles.card]}
                                         source={experience.photo}
                                     />
+                                    <Like></Like>
+
                                     <Text style={[styles.cardTitle, { color: experience.color }]}>{experience.type}</Text>
                                     <Text ellipsizeMode='tail' numberOfLines={2} style={[styles.cardDesc, { width: 150 }]}>{experience.title}</Text>
                                     <Text style={[styles.cardPrice]}>{experience.price}$ {experience.priceType}</Text>
---
+++ src/components/Like.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+
+    TouchableOpacity,
+
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+export default class Like extends React.Component {
+
+    state = {
+        iconName: "heart-o",
+        color: "white"
+    }
+
+
+    render() {
+
+        return (
+
+            <TouchableOpacity onPress={() => {
+                if (this.state.iconName == "heart-o") {
+                    this.setState({ iconName: "heart" })
+                    this.setState({ color: "#DC143C" })
+                } else {
+                    this.setState({ iconName: "heart-o" })
+                    this.setState({ color: "white" })
+                }
+
+            }} style={{ position: "absolute", right: 30, top: 10, zIndex: 9999 }}>
+                < Icon name={this.state.iconName} size={20} style={{ color: this.state.color }}></Icon >
+            </TouchableOpacity>
+        )
+    }
+}
